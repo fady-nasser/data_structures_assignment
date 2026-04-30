efd9250071f4d03d40f97d6de041cb4f2f5ebd8a
فادي ناصر عزيز سيدهم <fadynasseraziz@gmail.com>
2026-04-30 13:57:53 +0300

    corrected my understanding about the .h and .cpp file

--- LinkedList.h
+++ LinkedList.h
@@ -7,112 +7,16 @@ class LinkedList{
     Node* tail;
     int length;
     public:
-        LinkedList()
-        {
-            head = nullptr;
-            tail = nullptr;
-            length = 0;
-        }
-        //Implementing the methods responsible for adding nodes
-        void insertAtEnd(int val)
-        {
-            Node *newNode = new Node;
-            newNode->val = val;
-            newNode->next = nullptr;
-            tail->next = newNode;
-            tail = newNode;
-            length++;
-            return;
-        }
-        void insertAtHead(int val)
-        {
-            Node *newNode = new Node;
-            newNode->val = val;
-            newNode->next = head;
-            head = newNode;
-            length++;
-            return;
-        }
-        void add(int val, int index)
-        {
-            if(index >= length)
-            {
-                cout << "Index out of bounds";
-                return;
-            }
-            //Checking if we can take an easier route
-            else if(index == length - 1)
-            {
-                insertAtEnd(val);
-                return;
-            }
-            else if(index == 0)
-            {
-                insertAtHead(val);
-                return;
-            }
-            //Add the new Node
-            Node *newNode = new Node;
-            newNode->val = val;
-            Node* tmp = head;
-            for(int i=0; i<length; i++)
-            {
-                tmp = tmp->next;
-                if(i == index-1)
-                {
-                    newNode->next = tmp->next;
-                    tmp->next = newNode;
-                    length++;
-                    return;
-                }
-            }
-        }
-        //Implementing the methods responsible for removing nodes
-        void remove_first()
-        {
-            Node* new_head = head->next;
-            delete head;
-            head = new_head;
-            return;
-        }
-        void deleteValue(int index)
-        {
-            if(index >= length)
-            {
-                cout << "Index out of bounds";
-                return;
-            }
-            //Checking if we can take an easier route
-            if(index == 0)
-            {
-                remove_first();
-                return;
-            }
-            //Removing the node
-            Node* tmp = head;
-            Node* before_tmp = head;
-            for(int i = 0; i < length; i++)
-            {
-                before_tmp = before_tmp->next;
-                if(i == index-1)
-                {
-                    tmp = before_tmp->next;
-                    before_tmp->next = tmp->next;
-                    delete tmp;
-                    length--;
-                    return;
-                }
-            }
-        }
-        void remove_last()
-        {
-            deleteValue(length-1);
-            return;
-        }
-        int get_length()
-        {
-            return length;
-        }
+        LinkedList();
+        //Defining the methods responsible for adding nodes
+        void insertAtEnd(int val);
+        void insertAtHead(int val);
+        void add(int val, int index);
+        //Defining the methods responsible for removing nodes
+        void remove_first();
+        void deleteValue(int index);
+        void remove_last();
+        int get_length();
 };
 
 #endif
